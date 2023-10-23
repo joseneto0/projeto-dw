49a22c868e76298805d74894b432626375e1d2a6
José Neto <joseneto.jn1606@gmail.com>
2023-10-23 13:13:26 -0300

    Modificacoes nos arquivos + insercao do json server

--- public/js/principal.js
+++ public/js/principal.js
@@ -1,3 +1,20 @@
-function ls(){
-    
+const apiUrl = 'http://localhost:3000';
+
+async function fetchData(endpoint) {
+  try {
+    const response = await fetch(`${apiUrl}/${endpoint}`);
+    const data = await response.json();
+    console.log(data);
+    for (const file of data){
+        let div = document.createElement("div");
+        div.className = "itensLeft";
+        div.textContent = file.arquivo;
+        document.querySelector(".dinamicoLeft").appendChild(div);
+    }
+    return data;
+  } catch (error) {
+    console.error('Erro ao buscar dados:', error);
+  }
 }
+
+fetchData("filesClient");
--- readme.md
+++ readme.md
@@ -1,16 +1,18 @@
 # FrontEnd - Projeto DW
-Etapa do FrontEnd da cadeira "Desenvolvimento Web", feito utilizando Vite com o JavaScript Vanilla, CSS Grid/Flex Layout, BootStrap
+Etapa do FrontEnd da cadeira "Desenvolvimento Web", feito utilizando Vite com o JavaScript Vanilla, CSS Grid/Flex Layout, BootStrap, Json Server
 
 Pacotes necessários para rodar projeto:
 
 ```bash
   npm install
   npm install bootstrap
+  npm install json-server
 ```
 
 Comando para inicializar o projeto:
 
 ```bash
+  npx json-server --watch db.json
   npm run dev
 ```
 
